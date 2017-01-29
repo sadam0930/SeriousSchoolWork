0e5e42c3dfa9a9d3d0de6955ddde6fe11e0e4779
sadam0930 <s.adam0930@gmail.com>
2017-01-29 15:42:09 -0500

    2D maze solver

--- Social Multiplayer Games/HW0.js
+++ Social Multiplayer Games/HW0.js
@@ -1,17 +1,91 @@
-var map = [[""]];
+// let map = [
+// 			["","x"],
+// 			["",""],
+// 			["x",""]
+// 		  ];
 var output = [];
 
-var curNode = new Object();
-curNode.x = 0;
-curNode.y = 0;
-curNode.value = map[curNode.x][curNode.y]
+class Node {
+	constructor(y, x) {
+		this.y = y;
+		this.x = x;
+		this.discovered = false;
+	}
+}
+
+function isValidMove(map, nodeMap, node){
+	// console.log(nodeMap);
+	if(node.y > map.length - 1 || node.y < 0){
+		return false;
+	} else if(node.x > map[0].length - 1 || node.x < 0){
+		return false;
+	} else if(map[node.y][node.x] === "x"){
+		return false;
+	} else if(nodeMap[node.y][node.x].discovered === true){
+		return false;
+	} else {
+		return true;
+	}
+}
+
+function getAdjacentEdges(node){
+	return { 
+		right : new Node(node.y, node.x + 1),
+		down: new Node(node.y + 1, node.x),
+		left: new Node(node.y, node.x - 1),
+		up: new Node(node.y - 1, node.x)
+ 	}
+}
+
+function recursiveSearch(map, nodeMap, curNode){
+	let found = false;
+	nodeMap[curNode.y][curNode.x].discovered = true;
+	output.push([curNode.y, curNode.x]);
+
+	if(curNode.y === map.length - 1 && curNode.x === map[0].length - 1) {
+		return true;
+	} else {
+		let edges = getAdjacentEdges(curNode);
+		for (var key in edges){
+			let next = edges[key];
+			if(isValidMove(map, nodeMap, next) === true){
+				found = recursiveSearch(map, nodeMap, next);
+				if(found === false){
+					output.pop();
+					continue;
+				} else {
+					break;
+				}
+			}
+		}
+		return found;
+	}
+}
 
 function findPath(map){
-	if(!map) return null;
-	if(curNode.value === "x") { 
+	output = [];
+	let pathFound = false;
+	if(!map){
 		return null;
-	} else {
-		output.push([curNode.x, curNode.y]);
+	}
+	//recreate the map with each element being a node
+	let nodeMap = map.map(function(arr) {
+	    return arr.slice();
+	});
+	nodeMap.forEach(function(firstDimension, y){
+		firstDimension.forEach(function(value, x){
+			nodeMap[y][x] = new Node(y, x);
+		});
+	});
+	let curNode = nodeMap[0][0];
+	//check if there is an opening to the maze
+	if(map[curNode.y][curNode.x] === "x"){
+		return null;
+	}
+	pathFound = recursiveSearch(map, nodeMap, curNode);
+	if(pathFound === true){
 		return output;
+	} else {
+		return null;
 	}
 }
